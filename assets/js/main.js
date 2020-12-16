56f386eccba583de963eaec5b79f60b65c3d5928
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-16 12:51:54 +0100

    Aggiunto un v-model.lazy all'imput inmodo da cambiare il valore di li
    Aggiunto un v-model.lazy all'imput inmodo da cambiare il valore di li

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
+// Istruzioni:
+// Create una todo list usando VueJS.
+// Potete dare sfogo alla creativitá e per quanto riguarda l'HTML e il CSS.
+// Se non sapere che fare, di seguito trovate uno screenshot.
+// Funzionalitá:
+// La nostra todo list avrá alcune tasks di default predefinite
+// L'utente puó inserire nuove tasks
+// Cliccando sulla "X" l'utente puó cancellare una task
+// Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+// Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+// Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
 let app = new Vue({
     el: "#app", 
     data:{
-        
+       message: "",
+    },
+
+    methods:{
+        inserisci(){
+            //console.log("button clicked");
+        }
     }
+    
 });
