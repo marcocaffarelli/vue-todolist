4d02099b6e867b89ffd8f3228ced7d4b75d485b1
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-17 10:11:19 +0100

    Creata una cartella bonus
    Creata una cartella bonus

---
+++ bonus/assets/js/main.js
@@ -0,0 +1,38 @@
+// Istruzioni:
+// L'utente vuole poter modificare una task giá inserita
+// ma vuole anche poter indicare che la task é stata completata
+// inoltre se una task é stata completa allora vuole che venga inserita in un'altra colonna tipo "tasks completate"
+// ah non é finita, dice che quando cancella una task, non vuole che questa venga subito rimossa, ma vuole che resti visibile ma venga spostata in una colonna tipo "cestino"
+// si, l'utente é un ropi scatole, dice infine che vuole poter rimuovere tutte le tasks nel cestino cliccando su un pulsante tipo "svuota cestino"
+
+let app = new Vue({
+    el: "#app", 
+    data:{
+        //Array di stdenti di default
+       studenti: [
+          {text:"Mario Rossi"},
+          {text:"Luca Verdi"},
+          {text:"Elena Bianchi"}
+       ],
+       //valore iniziale dello studente che verrà creato nel input text
+       studente: "",
+    },
+ 
+    methods:{
+        inserisci(){
+            //console.log("button clicked");
+            // se l'elemento ha una lunghezza minore di 4 caratteri non verrà inserito nell'array
+            //(this.studente.length < 4) ? null : this.studenti.unshift({text:this.studente});
+            if(this.studente.length < 4){
+            }else {
+                //se'elemento ha più di 3 caratteri verrà inserito nell'array
+                this.studenti.unshift({text:this.studente});
+            }
+        },
+        elimina(studente){
+            //Cliccando nell'icona lo studente verra eliminato dall'array
+            this.studenti.splice(studente, 1)
+        }
+    }
+    
+});
